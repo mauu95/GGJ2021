6bcdffccaf2d0027898fb8c1a38f1de847c08aee
iTzNikk <nicolo.buganza@studenti.unimi.it>
2021-01-30 17:17:36 +0100

    Added Main Menu

---
+++ GGJ2021Source/Assets/Scripts/TitleTween.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class TitleTween : MonoBehaviour
+{
+    public float tweenTime;
+
+
+    private void Start()
+    {
+        Tween();
+    }
+
+    private void Tween()
+    {
+        LeanTween.cancel(gameObject);
+
+        LeanTween.scale(gameObject, new Vector3(3,3,3), tweenTime).setEasePunch();
+    }
+}
