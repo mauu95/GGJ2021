efdd0e3c096cbe29dc8962a205e9556d1d2ca353
Helium5t <10533138@polimi.it>
2021-01-30 16:53:46 +0100

    Finalized some things

--- GGJ2021Source/Assets/Scripts/Bullet.cs
+++ GGJ2021Source/Assets/Scripts/Bullet.cs
@@ -9,7 +9,13 @@ public class Bullet : MonoBehaviour
     private float timeToLive =20f;
     public virtual void ShootBehaviour(Vector3 spawnPoint,Vector3 shootDir){
         origin = spawnPoint;
-        direction = shootDir;
+        direction = shootDir.normalized;
         Destroy(gameObject,timeToLive);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")){
+            Destroy(gameObject);
+        }
     }
 }
