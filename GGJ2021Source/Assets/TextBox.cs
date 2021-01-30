d99e7c2b3878536c51055999f688a5a964a5c3bb
mauu95 <mauu95@hotmail.it>
2021-01-30 16:07:51 +0100

    Added textbox

---
+++ GGJ2021Source/Assets/TextBox.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBox : MonoBehaviour
+{
+    #region Singleton
+    public static TextBox instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+            return;
+
+        instance = this;
+    }
+
+    #endregion
+
+    public Sprite[] characterSprites;
+    public TextMeshProUGUI text;
+    public Image charIMG;
+
+    private void Start()
+    {
+        SetText("Questo è un messaggio edidato via script");
+        SetCharIMG(0);
+    }
+
+    public void SetText(string text)
+    {
+        this.text.text = text;
+    }
+
+    public void SetCharIMG(int n)
+    {
+        if (n < 0 || n > characterSprites.Length)
+            charIMG.sprite = null;
+        charIMG.sprite = characterSprites[n];
+    }
+}
