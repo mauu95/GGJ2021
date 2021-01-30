e07c68c035cb920f44d59ffc120a74f93f5c5f84
mauu95 <mauu95@hotmail.it>
2021-01-30 13:21:47 +0100

    Added asteroids

---
+++ GGJ2021Source/Assets/Scripts/DestroyAfterSec.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterSec : MonoBehaviour
+{
+    public float seconds = 10;
+
+    private void Start()
+    {
+        StartCoroutine(Destroy());
+    }
+
+    private IEnumerator Destroy()
+    {
+        yield return new WaitForSeconds(seconds);
+        Destroy(this.gameObject);
+    }
+}
