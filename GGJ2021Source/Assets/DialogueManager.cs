330a3a7581d7fb765e25edebc933c868b5c6eb7a
mauu95 <mauu95@hotmail.it>
2021-01-30 22:18:28 +0100

    Added dialogue2-5

--- GGJ2021Source/Assets/DialogueManager.cs
+++ GGJ2021Source/Assets/DialogueManager.cs
@@ -47,6 +47,38 @@ public class DialogueManager : MonoBehaviour
     new Sentence("nettuno", "We'll see about that.")
     };
 
+    Sentence[] dialogue3 =
+    {
+    new Sentence("urano", "So it's true, we have a visitor in our System. I heard you have"),
+    new Sentence("urano", "already met some of my fellow planets. Let me guess, Neptune didn't let"),
+    new Sentence("urano", "you move along without putting up a fight."),
+    new Sentence("found", "I beat him. What's his problem?"),
+    new Sentence("urano", "Oh, he has quite a cold heart. But I'm proud to admit I'm the coldest."),
+    new Sentence("found", "You are?"),
+    new Sentence("urano", "I must be. How else would I survive all the jokes the other planets"),
+    new Sentence("urano", "tell each other about me? But now I must let you go."),
+    new Sentence("urano", "You still have a hard journey ahead of you.")
+    };
+
+    Sentence[] dialogue4 =
+    {
+    new Sentence("saturno", "I can't believe Uranus let you through. Not that you will be able to"),
+    new Sentence("saturno", "take down my majestic rings easily anyway."),
+    new Sentence("saturno", "Come, let's see if your core is strong enough to beat me.")
+    };
+
+    Sentence[] dialogue5 =
+    {
+    new Sentence("giove", "I see you managed to beat Saturn. Now listen to me: I'm the big boss"),
+    new Sentence("giove", "on this side of the Asteroid Belt, so let me give you a piece of advice."),
+    new Sentence("giove", "The Sun will never accept you in our System, and neither will most of"),
+    new Sentence("giove", "the planets you'll meet from now on. Still, if you don't cross the Belt,"),
+    new Sentence("giove", "maybe there's still a chance you can go back and find another galaxy."),
+    new Sentence("found", "But I like it here, some planets have been nice to me and I'm too tired"),
+    new Sentence("found", "to go back into deep space."),
+    new Sentence("giove", "Then I will have to stop you. Come, my moons, it's time to fight.")
+    };
+
     public void Play(Sentence sentence)
     {
         if (!tb)
