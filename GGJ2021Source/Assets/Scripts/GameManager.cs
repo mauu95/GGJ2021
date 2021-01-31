0c636c69335704de3a7edfc876e59d393b42c7a1
iTzNikk <nicolo.buganza@studenti.unimi.it>
2021-01-31 10:38:27 +0100

    Created PauseMenu GO and Prefab
    version 1

--- GGJ2021Source/Assets/Scripts/GameManager.cs
+++ GGJ2021Source/Assets/Scripts/GameManager.cs
@@ -1,20 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static bool IsGamePaused;
+    [SerializeField] private GameObject pauseMenu;
+
+    private void Start()
     {
-        
+        IsGamePaused = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.R))
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsGamePaused) ResumeGame();
+            else PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        IsGamePaused = true;
+        Time.timeScale = 0.0f;
+        pauseMenu.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        IsGamePaused = false;
+        Time.timeScale = 1.0f;
+        pauseMenu.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log(" QUITTING GAME");
     }
 }
