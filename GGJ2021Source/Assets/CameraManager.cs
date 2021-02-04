938514f11b1b41623fad4c8dd5c4860289de274e
mauu95 <mauu95@hotmail.it>
2021-02-04 22:46:37 +0100

    Added my camera manager

---
+++ GGJ2021Source/Assets/CameraManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Transform toFollow;
+    private float smoothSpeed = 0.125f;
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPosition = toFollow.position;
+        desiredPosition.z = -1;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        transform.position = smoothedPosition;
+    }
+}
