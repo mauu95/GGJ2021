956b15fb034b25af0b26b21c86083b0b362ac8fc
mauu95 <mauu95@hotmail.it>
2021-03-12 23:51:32 +0100

    FIXED impact anim

--- GGJ2021Source/Assets/impact.cs
+++ GGJ2021Source/Assets/impact.cs
@@ -6,7 +6,6 @@ public class impact : MonoBehaviour
 {
     public float delay = 0f;
 
-    // Use this for initialization
     void Start()
     {
         Destroy(gameObject, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length + delay);
