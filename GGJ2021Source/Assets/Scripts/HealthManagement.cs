c0102d813e05f3a8b1733fc42d7949136be26d38
mauu95 <mauu95@hotmail.it>
2021-04-23 23:12:57 +0200

    Re-added: Invulnerability time

--- GGJ2021Source/Assets/Scripts/HealthManagement.cs
+++ GGJ2021Source/Assets/Scripts/HealthManagement.cs
@@ -8,6 +8,7 @@ public class HealthManagement : MonoBehaviour
 {
     [SerializeField] public int health;
     private HPGFXManager HPgfx;
+    private bool invulnerable;
 
     private void Start()
     {
@@ -22,16 +23,26 @@ public class HealthManagement : MonoBehaviour
 
     public void RemoveHealth()
     {
+        if (invulnerable)
+            return;
+
         health--;
         HPgfx.SetHealth(health);
         FMODUnity.RuntimeManager.PlayOneShot("event:/Player/Hit", transform.position);
-        if (health < 0)
-        {
-            print("YOU LOST");
-            SceneManager.LoadScene("GameOver");
-        }
 
+        if (health < 0)
+            SceneManager.LoadScene("GameOver");
+
+        StartCoroutine(ActivateInvulnerability());
     }
+
+    private IEnumerator ActivateInvulnerability()
+    {
+        invulnerable = true;
+        yield return new WaitForSeconds(0.5f);
+        invulnerable = false;
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Enemy"))
